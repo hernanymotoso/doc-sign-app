5134183badbf2c0a5f739e755fda4f74bfb4cc9b
Hernany Motoso <hernanymotoso@gmail.com>
2025-04-01 23:25:27 -0300

    chore: config eslint, prettier and fix tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:tailwindcss/recommended', 'prettier'),
+  ...compat.plugins('prettier', 'tailwindcss'),
+  {
+    rules: {
+      'prettier/prettier': ['error', { printWidth: 120 }],
+    },
+    ignores: ['build/*', 'dist/*', 'node_modules/*', 'config/*', '*.ico', '*.png', '*.jpg', '*.jpeg', '*.gif', '*.svg'],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
