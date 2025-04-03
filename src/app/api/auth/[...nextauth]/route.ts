75f360bfe728cb6b2efe8c14a47d8cd6df17aace
Hernany Motoso <hernanymotoso@gmail.com>
2025-04-03 18:50:26 -0300

    refactor: move authOptions to own file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,6 @@
-import { db } from '@/db'
-import { findUserByEmail } from '@/db/queries/users'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { compare } from 'bcrypt'
+import { authOptions } from '@/lib/auth'
 import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import GithubProvider from 'next-auth/providers/github'
 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
-
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-  throw new Error('Missing github oauth credentials')
-}
-
-const handler = NextAuth({
-  adapter: PrismaAdapter(db),
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/sign-in',
-  },
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email', placeholder: 'john@example.com' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null
-        const existingUser = await findUserByEmail(credentials.email)
-        if (!existingUser) return null
-        const passwordsMatch = await compare(credentials.password, existingUser.password ?? '')
-        if (!passwordsMatch) return null
-
-        return {
-          id: existingUser.id,
-          email: existingUser.email,
-          name: existingUser.name,
-        }
-      },
-    }),
-    GithubProvider({
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session }) {
-      return session
-    },
-  },
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
