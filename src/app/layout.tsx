cd094b908d5d7d9775091a38a6304ae876dbb924
Hernany Motoso <hernanymotoso@gmail.com>
2025-04-03 18:51:15 -0300

    feat: create header and session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
+import { Header } from '@/components/Header'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
 import './globals.css'
+import { SessionProvider } from '@/lib/sessionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,12 +14,17 @@ export const metadata: Metadata = {
   description: 'Plataforma de assinatura de documentos online',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
-        {children}
+        <SessionProvider>
+          {session?.user && <Header />}
+          <div className={session?.user ? 'pt-16' : undefined}>{children}</div>
+          <Toaster position="top-right" />
+        </SessionProvider>
       </body>
     </html>
   )
